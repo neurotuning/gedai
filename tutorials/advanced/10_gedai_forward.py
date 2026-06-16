a25e3fe91fb1f51a4acb6d52d565f42176cea943
Victor Férat <victor.ferat@live.Fr>
2026-06-16 16:37:42 +0200

    Update example dataset

--- tutorials/advanced/10_gedai_forward.py
+++ tutorials/advanced/10_gedai_forward.py
@@ -9,11 +9,10 @@ matrix using a :class:`mne.Forward` solution.
 # %%
 
 import mne
-from mne.datasets import eegbci, fetch_fsaverage
-
-from gedai import Gedai, MultibandGedai
+from mne.io import read_raw
+from mne.datasets import fetch_fsaverage
+from gedai.data import get_contaminated_eeg_set_path
 from gedai.covariance import compute_covariance_from_forward
-from gedai.viz import plot_mne_style_overlay_interactive
 
 # %%
 # Download fsaverage files
@@ -28,15 +27,11 @@ bem = fs_dir / "bem" / "fsaverage-5120-5120-5120-bem-sol.fif"
 
 # %%
 # Load EEG data
-(raw_fname,) = eegbci.load_data(subjects=1, runs=[6])
-raw = mne.io.read_raw_edf(raw_fname, preload=True)
-eegbci.standardize(raw)
-montage = mne.channels.make_standard_montage("standard_1005")
-raw.set_montage(montage)
-raw.crop(0, 15)
-raw.pick("eeg").load_data().apply_proj()
-raw.set_eeg_reference("average", projection=False)
+raw = read_raw(str(get_contaminated_eeg_set_path()), preload=True)
 
+# %%
+# Set the EEG electrode locations
+raw.set_montage("standard_1005")
 
 # %%
 # Check that the locations of EEG electrodes is correct with respect to MRI
