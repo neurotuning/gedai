2a7ed414c82adf016328a58cb93dcd5627c6a2bd
neurotuning-personal <neurotuning-personal@users.noreply.github.com>
2026-09-02 21:28:40 +0000

    Deploy preview for PR 158 🛫

---
+++ pr-preview/pr-158/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.5.0.dev51+g7e660a2b0',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: false,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
---
+++ pr-preview/pr-158/dev/changes/index.rst
@@ -0,0 +1,14 @@
+.. _changelog:
+
+Changelog
+=========
+
+.. toctree::
+    :titlesonly:
+
+    latest.rst
+    0.4.0
+    0.3.0
+    0.2.0
+    0.1.0
+
