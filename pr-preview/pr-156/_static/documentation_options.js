0364154d018c2b1785e9a1115e0c270dfe01e547
neurotuning-personal <neurotuning-personal@users.noreply.github.com>
2026-09-02 11:59:57 +0000

    Deploy preview for PR 156 🛫

--- pr-preview/pr-156/_static/documentation_options.js
+++ pr-preview/pr-156/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.4.0.dev40+g2730f6129',
+    VERSION: '0.4.0.dev41+gde9b5972e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
